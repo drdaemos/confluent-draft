ca818342590af109b69b8ffd8c3d4d515362b14d
Eugene Dementjev <drdaemos@gmail.com>
2015-05-25 07:23:34 +0300

    some shit changes in header

--- src/scripts/components/Header.js
+++ src/scripts/components/Header.js
@@ -9,8 +9,23 @@ require('../../styles/header.less');
 var Header = React.createClass({
   render: function() {
     return (
-      <header className='header'>
-      	<p>Диплом, который пишет не Джек.</p>
+      <header className='header ui menu page grid inverted'>
+      	<div className='container'>
+      		<a className='launch item'>
+		      <i className='content icon'></i>
+		      <span> Menu </span>
+		    </a>
+		    <div className='right menu'>
+		    	<div className="ui dropdown">
+				  <div className="text">james@rolfe.com</div>
+				  <i className="dropdown icon"></i>
+				  <div className="menu">
+				    <div className="header">Profile</div>
+				    <div className="item">Logout</div>
+				  </div>
+				</div>
+		    </div>
+      	</div>
       </header>
     );
   }
